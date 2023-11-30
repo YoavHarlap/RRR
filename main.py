43eb281a6bdc898bbbe32acc43d061cc30135d95
yoavharlap <yoavharlap1@gmail.com>
2023-11-30 13:31:09 +0200

    uupdate requirements.txt

--- main.py
+++ main.py
@@ -3,10 +3,12 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+import numpy as np
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'yoavvvvv, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    x = np.array(123)
+    print(x)
 
 
 # Press the green button in the gutter to run the script.
