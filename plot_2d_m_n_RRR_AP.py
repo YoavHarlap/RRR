aae8800245f386d64a67281ab86b9cefd7dd131f
yoavharlap <yoavharlap1@gmail.com>
2024-01-07 02:29:00 +0200

    motsash

--- plot_2d_m_n_RRR_AP.py
+++ plot_2d_m_n_RRR_AP.py
@@ -15,7 +15,7 @@ try:
         data_text = file.read()
 except FileNotFoundError:
     # Handle file not found error by trying another location
-    new_file_path = r"/home/yoavharlap/PycharmProjects/RRR/saves.txt"
+    new_file_path = r"/home/yoavharlap/PycharmProjects/RRR/saves1.txt"
     try:
         with open(new_file_path, "r") as file:
             data_text = file.read()
@@ -24,33 +24,34 @@ except FileNotFoundError:
 
 
 print(data_text)
+# Ignore lines containing "The projections is not same" before processing
+data_text = '\n'.join(line for line in data_text.split('\n') if "The projections is not same" not in line)
 
 # Extract relevant information using regular expressions
 pattern = re.compile(r"m = (\d+), n = (\d+)\n(?:alternating_projections Converged in (\d+) iterations\.)?(?:\nRRR_algorithm Converged in (\d+) iterations\.)?")
 matches = pattern.findall(data_text)
+
 # Convert matches to dictionary
 data = []
 for match in matches:
     m, n, ap_iterations, rrr_iterations = map(lambda x: int(x) if x else None, match)
     data.append({'m': m, 'n': n, 'AP_iterations': ap_iterations, 'RRR_iterations': rrr_iterations})
 
-# print(data)
 # Plotting
 colors = []
 for entry in data:
-    if entry['AP_iterations'] and entry['RRR_iterations']:
+    if entry['AP_iterations'] is not None and entry['RRR_iterations'] is not None:
         colors.append('green')
-    elif not entry['AP_iterations'] and not entry['RRR_iterations']:
+    elif entry['AP_iterations'] is None and entry['RRR_iterations'] is None:
         colors.append('red')
-    elif entry['AP_iterations'] and not entry['RRR_iterations']:
+    elif entry['AP_iterations'] is not None and entry['RRR_iterations'] is None:
         colors.append('blue')  # Choose your color for AP Converged only
-    elif not entry['AP_iterations'] and entry['RRR_iterations']:
+    elif entry['AP_iterations'] is None and entry['RRR_iterations'] is not None:
         colors.append('orange')  # Choose your color for RRR Converged only
 
 for i, entry in enumerate(data):
     plt.scatter(entry['m'], entry['n'], color=colors[i])
-    plt.text(entry['m'], entry['n'], f"AP: {entry['AP_iterations']},"
-                                     f" RRR: {entry['RRR_iterations']}", fontsize=4)
+    plt.text(entry['m'], entry['n'], f"AP: {entry['AP_iterations']}, RRR: {entry['RRR_iterations']}", fontsize=8)
 
 plt.title('Convergence Plot')
 plt.xlabel('m')
