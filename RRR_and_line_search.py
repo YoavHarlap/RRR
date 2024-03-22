c800951f109c14709a98a1f0307d9a0de1fd254e
yoavharlap <yoavharlap1@gmail.com>
2024-03-22 17:24:31 +0200

    line search after cleaning

--- RRR_and_line_search.py
+++ RRR_and_line_search.py
@@ -20,11 +20,10 @@ def objective_function(y):
     result1 = squared_euclidean_norm(y, PAPB_y)
     result2 = squared_euclidean_norm(y, P_Ay)
     result3 = squared_euclidean_norm(y, P_By)
-
     return result1 - 0.5 * (result2 + result3)
 
 
-def gradient(y,A,b):
+def gradient(y, A, b):
     P_Ay = PA(y, A)
     P_By = PB(y, b)
     PAPB_y = PA(P_By, A)
@@ -41,29 +40,17 @@ def phase(y):
 def PB(y, b):
     # Calculate the phase of the complex vector y
     phase_y = phase(y)
-
     # Point-wise multiplication between b and phase_y
     result = b * phase_y
-
     return result
 
 
 def PA(y, A):
     # Calculate the pseudo-inverse of A
     A_dagger = np.linalg.pinv(A)
-
     # Matrix-vector multiplication: AA†y
     result = np.dot(A, np.dot(A_dagger, y))
-
     return result
-
-
-def step_gd(A, b, y, beta):
-    P_Ay = PA(y, A)
-    P_By = PB(y, b)
-    PAPB_y = PA(P_By, A)
-    y = y - beta * (-2 * PAPB_y + P_Ay + P_By)
-    return y
 
 
 def step_RRR(A, b, y, beta):
@@ -92,8 +79,15 @@ def armijo_line_search(y, grad, alpha=0.5, beta=0.5, max_iter=100):
     return t
 
 
+def step_line_search(A, b, y):
+    grad = gradient(y, A, b)
+    # obj_func = objective_function(y)
+    learning_rate = armijo_line_search(y, grad)
+    y_new = y - learning_rate * grad
+    return y_new
 
-def run_algorithm(A, b, y_init, algo, beta=None, max_iter=100, tolerance=1e-6,alpha=0.5):
+
+def run_algorithm(A, b, y_init, algo, beta=None, max_iter=100, tolerance=1e-6, alpha=0.5):
     # Initialize y with the provided initial values
     y = y_init
 
@@ -136,107 +130,37 @@ def run_algorithm(A, b, y_init, algo, beta=None, max_iter=100, tolerance=1e-6,al
             if norm_diff < tolerance:
                 print(f"{algo} Converged in {iteration + 1} iterations.")
                 break
-    # elif algo == "line_search":
-
-    #     lr_param = 0.8
-    #     iteration = 0
-    #     objective_function_array = []
-    #     while iteration < max_iter:
-    #         # grad = gradient(y)
-    #         # learning_rate = 1.0  # Initialize learning rate to 1
-    #         # Backtracking line search
-    #         # while np.linalg.norm(objective_function(y - learning_rate * grad)) > np.linalg.norm(objective_function(y)):
-    #         #     learning_rate *= lr_param
-    #         # y = y - learning_rate * grad
-
-        
-    #         learning_rate = 0.5
-    #         grad =  gradient(y,A,b)
-    #         value = objective_function(y)
-        
-    #         while objective_function(y - learning_rate * grad) > value - alpha * learning_rate * np.dot(grad, grad):
-    #             learning_rate *= lr_param
-        
-    #         y = y - learning_rate * grad
-    
-    
-    #         # print("learning_rate:", learning_rate)
-    #         # print("objective_function:", objective_function(y))
-
-    #         objective_function_array.append(objective_function(y))
-            
-    #         plt.plot(abs(y), label='Iter_RRR_line_search')
-    #         plt.plot(b, label='b')
-
-    #         # Adding labels and legend
-    #         plt.xlabel('element')
-    #         plt.ylabel('value')
-    #         plt.title(f'Plots learning_rate = {learning_rate}')
-    #         plt.legend()
-
-    #         # Display the plot
-    #         plt.show()
-
-            
-
-    #         # Calculate the norm difference between PB - PA
-    #         norm_diff = np.linalg.norm(abs(PB(y, b)) - abs(PA(y, A)))
-
-    #         # Store the norm difference for plotting
-    #         norm_diff_list.append(norm_diff)
-    #         iteration=iteration+1
-    #         # Check convergence
-    #         if norm_diff < tolerance:
-    #             print(f"{algo} Converged in {iteration + 1} iterations.")
-    #             break
-
 
     elif algo == "line_search":
+
         objective_function_array = []
         learning_rate = 1  # Initial learning rate
         for iteration in range(max_iter):
-            grad = gradient(y, A, b)
-            # obj_func = objective_function(y)
-            
-            # Perform Armijo line search
-            learning_rate = armijo_line_search(y, grad)
-    
-            # Update solution
-            y_new = y - learning_rate * grad
-    
+            y = step_line_search(A, b, y)
             # Calculate the norm difference between PB - PA
-            norm_diff = np.linalg.norm(PB(y_new, b) - PA(y_new, A))
-    
+            norm_diff = np.linalg.norm(PB(y, b) - PA(y, A))
             # Store the norm difference for plotting
             norm_diff_list.append(norm_diff)
-           
+            objective_function_array.append(objective_function(y))
+
             # Check convergence
             if norm_diff < tolerance:
                 print(f"{algo} Converged in {iteration + 1} iterations.")
                 break
-    
-            # Update solution for next iteration
-            y = y_new
-            objective_function_array.append(objective_function(y))
-            # plt.plot(abs(y), label='Iter_RRR_line_search')
-            
+
             if iteration % 100 == 0:
                 print("norm_diff: ", norm_diff)
                 plt.plot(abs(PA(y, A)), label=f'Iter_RRR_line_search_{iteration}')
                 plt.plot(b, label='b')
-    
+
                 # Adding labels and legend
                 plt.xlabel('element')
                 plt.ylabel('value')
                 plt.title(f'Plots learning_rate = {learning_rate}')
                 plt.legend()
-    
+
                 # Display the plot
                 plt.show()
-
-
-        
-
 
         # Plot the objective function values
         plt.plot(objective_function_array, marker='o', linestyle='-', color='b')
@@ -246,7 +170,6 @@ def run_algorithm(A, b, y_init, algo, beta=None, max_iter=100, tolerance=1e-6,al
         plt.grid(True)
         plt.show()
 
-
     # Plot the norm difference over iterations
     plt.plot(norm_diff_list)
     plt.xlabel('Iteration')
@@ -254,53 +177,7 @@ def run_algorithm(A, b, y_init, algo, beta=None, max_iter=100, tolerance=1e-6,al
     plt.title(f'Convergence of {algo} Algorithm')
     plt.show()
 
-
-    # print("y:", y[:5])
-    # print("abs y:", np.abs(y[:5]))
-
     return y
-
-
-def line_search_RRR(A, b, y, max_iter=100, tolerance=1e-6):
-    # Set the initial step size
-    beta = 1.0
-
-    for iteration in range(max_iter):
-        # Calculate the gradient of the objective function
-        gradient = -2 * PA(PB(y, b), A) + PA(y, A) + PB(y, b)
-
-        # Update y with the current step size
-        y_new = y + beta * gradient
-
-        # Calculate the norm difference between PB - PA for the updated y
-        norm_diff_new = np.linalg.norm(PB(y_new, b) - PA(y_new, A))
-
-        # Check if the new step size improves the objective function
-        if norm_diff_new < tolerance:
-            print("Line search converged.")
-            return y_new, beta
-
-        # Reduce the step size (backtracking)
-        beta *= 0.5
-
-    print("Line search did not converge. Returning the last result.")
-    return y_new, beta
-
-
-def step_RRR_with_line_search(A, b, y, max_iter=100, tolerance=1e-6):
-    # Perform line search to find the optimal step size
-    y, beta = line_search_RRR(A, b, y, max_iter, tolerance)
-
-    # Update y with the optimal step size
-    P_Ay = PA(y, A)
-    P_By = PB(y, b)
-    PAPB_y = PA(P_By, A)
-    y = y + beta * (2 * PAPB_y - P_Ay - P_By)
-
-    return y
-
-
-# Modify run_algorithm function to use the new step function
 
 
 log_file_path = os.path.join("texts", "RRR_and_GD.txt")
@@ -314,14 +191,13 @@ beta = 0.5
 max_iter = 10000
 tolerance = 1e-6
 
-
 array_limit = 200
 m_array = np.arange(10, array_limit + 1, 10)
 n_array = np.arange(10, array_limit + 1, 10)
 
 #
 m_array = [20]
-n_array = [11]
+n_array = [10]
 
 # Loop over different values of m and n
 for m in m_array:  # Add more values as needed
@@ -343,47 +219,32 @@ for m in m_array:  # Add more values as needed
         y_true = np.dot(A, x)
         y_true_real = np.dot(A_real, x_real)
 
-        # print("y_true:", y_true[:5])
-        # print("y_true_real:", y_true_real[:5])
-
         # Initialize y randomly
         y_initial = np.random.randn(m) + 1j * np.random.randn(m)
         y_initial_real = np.random.randn(m)
-
-        # print("y_initial:", y_initial[:5])
-        # print("y_initial_real:", y_initial_real[:5])
-
-        # # Epsilon value
-        # epsilon = 1e-1
-        # epsilon = 0.5
-        # y_initial = y_true + epsilon
 
         A = A_real
         b = b_real
         y_initial = y_initial_real
         y_true = y_true_real
 
-        # Call the alternating_projections function with specified variance, standard deviation, and initial y
         # result_AP = run_algorithm(A, b, y_initial, algo="alternating_projections", max_iter=max_iter,
         #                           tolerance=tolerance)
         # print("result_AP:", np.abs(result_AP[:5]))
         # print("b:        ", b[:5])
 
-        # # Call the RRR_algorithm function with specified parameters
         # result_RRR = run_algorithm(A, b, y_initial, algo="RRR_algorithm", beta=beta, max_iter=max_iter,
         #                            tolerance=tolerance)
         # print("result_RRR:", np.abs(result_RRR[:5]))
         # print("b:         ", b[:5])
 
-        result_RRR_line_search = run_algorithm(A, b, y_initial, algo="line_search", max_iter=max_iter,
-                                               tolerance=tolerance)
-        print("result_RRR_line_search:", np.abs(PA(result_RRR_line_search,A)[:5]))
+        result_line_search = run_algorithm(A, b, y_initial, algo="line_search", max_iter=max_iter,
+                                           tolerance=tolerance)
+        print("result_line_search:", np.abs(PA(result_line_search, A)[:5]))
         print("b:                     ", b[:5])
 
-
-        plt.plot(abs(PA(result_RRR_line_search,A)), label='result_RRR_line_search')
+        plt.plot(abs(PA(result_line_search, A)), label='result_line_search')
         # plt.plot(abs(PA(result_RRR,A)), label='result_RRR')
-        # plt.plot(abs(PB(result_RRR, b)))
         # plt.plot(abs(PA(result_AP,A)), label='result_AP')
         plt.plot(b, label='b')
 
@@ -395,17 +256,3 @@ for m in m_array:  # Add more values as needed
 
         # Display the plot
         plt.show()
-        
-        
-        # plt.plot(y_true_real, label="y_true_real")
-        # plt.plot(result_RRR_line_search, label='y_result_RRR_line_search')
-
-        # # Adding labels and legend
-        # plt.xlabel('element')
-        # plt.ylabel('value')
-        # plt.title('Plot of Terms')
-        # plt.legend()
-
-        # # Display the plot
-        # plt.show()
-
